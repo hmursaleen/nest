4f779cdeeb9ba8f57eb08fd7e3e9fb74a3de8644
habibulmursaleen@gmail.com <habibulmursaleen@gmail.com>
2024-08-20 12:19:22 +0600

    configured postgresql

--- blog/settings.py
+++ blog/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogdb',  # Make sure this matches the database name you created
+        'USER': 'admin',  # Make sure this matches the username you created
+        'PASSWORD': 'admin',  # Replace with the password you set for the 'admin' user
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+
 }
 
 
