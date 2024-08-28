e0a88d3383d4e7fc42694f94073a6ecba64c4d1b
habibulmursaleen@gmail.com <habibulmursaleen@gmail.com>
2024-08-28 13:12:19 +0600

    added comment update feature

--- comments/urls.py
+++ comments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CommentCreateView
+from .views import CommentCreateView, CommentUpdateView
 
 app_name = 'comments'
 
 urlpatterns = [
-    path('post/<slug:slug>/comment/', CommentCreateView.as_view(), name='add_comment'),
+    path('comment/post/<slug:slug>', CommentCreateView.as_view(), name='add_comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),
 ]
--- comments/views.py
+++ comments/views.py
@@ -1,8 +1,12 @@
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Comment
 from .forms import CommentForm
 from blogs.models import BlogPost
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin 
+
+
+
 
 class CommentCreateView(CreateView):
     model = Comment
@@ -35,3 +39,32 @@ class CommentCreateView(CreateView):
 	get_post: Retrieves the post associated with the comment using the slug from the URL.
 	get_context_data: Passes the post object to the template context for displaying post-specific information.
 	'''
+
+
+
+
+
+
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'comment_update_form.html'
+
+    def get_success_url(self):
+        # Redirect back to the post detail page after the comment is updated
+        return reverse_lazy('blogs:post_detail', kwargs={'slug': self.object.post.slug})
+
+    def test_func(self):
+        # Ensure that the user is the author of the comment
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def handle_no_permission(self):
+        # Customize the response if the user fails the test (e.g., redirect to the post)
+        return reverse_lazy('blogs:post_detail', kwargs={'slug': self.get_object().post.slug})
+
+        '''
+        handle_no_permission Method: This method handles what happens if the user doesn't pass 
+        the test_func check. You can customize the response, such as redirecting the user back to the post detail page.
+        '''
