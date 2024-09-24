d0e5069ce05d02ae08b6c622aafae4f7191ab08d
habibulmursaleen@gmail.com <habibulmursaleen@gmail.com>
2024-09-24 17:56:11 +0600

    wrote test for APIs for CRUD operations on post of blogs app

--- blogs/tests.py
+++ blogs/tests.py
@@ -344,3 +344,76 @@ class TagDetailViewPaginationTest(TestCase):
         response = self.client.get(reverse('blogs:tag_detail', kwargs={'name': 'NoPostsTag'}))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No posts found for this tag.")
+
+
+
+
+
+
+
+
+
+
+
+from rest_framework.test import APITestCase
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class BlogPostAPITestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.tag = Tag.objects.create(name='django')
+        self.client.login(username='testuser', password='testpass')
+        self.token = RefreshToken.for_user(self.user).access_token
+        self.api_authentication()
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+
+    def test_create_blog_post(self):
+        data = {
+            "title": "Test Blog Post",
+            "content": "This is a test blog post.",
+            "tags": [self.tag.name]
+        }
+        response = self.client.post('/api/blogs/posts/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(BlogPost.objects.count(), 1)
+        self.assertEqual(BlogPost.objects.get().title, "Test Blog Post")
+
+    def test_get_blog_post(self):
+        blog_post = BlogPost.objects.create(
+            title="Existing Blog Post", content="This is an existing blog post.",
+            author=self.user
+        )
+        blog_post.tags.add(self.tag)
+        response = self.client.get(f'/api/blogs/posts/{blog_post.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], blog_post.title)
+
+    def test_update_blog_post(self):
+        blog_post = BlogPost.objects.create(
+            title="Update Blog Post", content="This is a blog post to be updated.",
+            author=self.user
+        )
+        blog_post.tags.add(self.tag)
+        data = {
+            "title": "Updated Blog Post",
+            "content": "This blog post has been updated.",
+            "tags": [self.tag.name]
+        }
+        response = self.client.put(f'/api/blogs/posts/{blog_post.id}/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        blog_post.refresh_from_db()
+        self.assertEqual(blog_post.title, "Updated Blog Post")
+
+    def test_delete_blog_post(self):
+        blog_post = BlogPost.objects.create(
+            title="Delete Blog Post", content="This blog post will be deleted.",
+            author=self.user
+        )
+        blog_post.tags.add(self.tag)
+        response = self.client.delete(f'/api/blogs/posts/{blog_post.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(BlogPost.objects.count(), 0)
--- comments/tests.py
+++ comments/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
-from blogs.models import BlogPost
+from blogs.models import BlogPost, Tag
 from comments.models import Comment
 
 class CommentCreateViewTest(TestCase):
